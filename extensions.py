9dc7d1315359c9c6a73fba7f641678a4ff5df97d
Grigory-Dripak <126253741+Grigory-Dripak@users.noreply.github.com>
2023-04-13 16:11:26 +0300

    source data writing into files

--- extensions.py
+++ extensions.py
@@ -1,42 +1,38 @@
 import requests
+import json
 import xml.etree.ElementTree as ET
+from os import getenv
+from dotenv import load_dotenv, find_dotenv
 
 class CurrencyConverter:
     url_cbr = 'https://cbr.ru/scripts/XML_daily.asp'
-    url_apilayer = 'https://api.apilayer.com/exchangerates_data/'
+    url_apilayer = 'https://api.apilayer.com/exchangerates_data/latest?base=RUB&symbols=EUR,USD'
+    legal_currency = dict(USD='доллар',
+                          EUR='евро',
+                          RUB='рубль')
 
-tree = ET.fromstringlist(cbr)
-c = tree.findall('Valute/CharCode')
-v = tree.findall('Valute/Value')
-cbr_date = tree.get('Date')
-cur_values = {}
-for i in range(len(c)):
-    if c[i].text in config.legal_currency:
-        cur_values[c[i].text]=v[i].text.replace(',', '.')
+    def get_price(self, base: str, quote: str, amount: float, key):
+        pass
 
-print(tree)
-print(cbr.content)
-print(cur_values['USD'])
-print(cur_values['CNY'])
-print(round(float(cur_values['USD'])/float(cur_values['CNY']), 4))
-print(cbr_date)
+    def get_apidata(self):
+        load_dotenv(find_dotenv())
+        req = json.loads(requests.get(self.url_apilayer, {'apikey': getenv('APIKEY')}).content)
 
+        with open('apilayer_dict.json', 'w') as file:
+            json.dump(req, file, indent=4, ensure_ascii=False)
 
-url = 'https://api.apilayer.com/exchangerates_data/'
+    def get_cbrdata(self):
+        cbr = requests.get(self.url_cbr)
+        cbr.encoding = 'windows-1251'
+        tree = ET.fromstringlist(cbr)
+        cbr_date = tree.get('Date')
+        cur_id = tree.findall('Valute/CharCode')
+        cur_value = tree.findall('Valute/Value')
+        cur_values = {}
+        for i in range(len(cur_id)):
+            if cur_id[i].text in self.legal_currency:
+                cur_values[cur_id[i].text]=cur_value[i].text.replace(',', '.')
 
-def get_data(url):
-    apilayer_key = {'apikey': 'HPZdNuGf6xt3PNnHxlzAM0wIWUyXGm7I'}
-    req = requests.get(url, apilayer_key)
-    req.encoding = 'windows-1251'
-
-    with open('apilayer_data.txt', 'w') as file:
-        file.write(req.text)
-
-currencies = []
-
-for c in legal_currency.keys():
-    if c != 'RUB':
-        currencies.append(c)
-currencies = ','.join(currencies)
-
-get_data(f'{url}latest?base=RUB&symbols={currencies}')
+        with open('cbr_data.txt', 'w') as file:
+            for i, j in cur_values.items():
+                file.write(f"Курс ЦБ от {cbr_date}: {i}={j}\n")
--- myfirstbot.py
+++ myfirstbot.py
@@ -1,10 +1,11 @@
 import telebot
-from time import time
 from os import getenv
 from dotenv import load_dotenv, find_dotenv
+import extensions
 
+calc = extensions.CurrencyConverter()
 load_dotenv(find_dotenv())
-
+# @mypersonal85_bot
 bot = telebot.TeleBot(getenv('TOKEN'))
 
 
@@ -15,12 +16,19 @@ def chating(message: telebot.types.Message):
 
 @bot.message_handler(commands=['start', 'help'])
 def trycommand(message: telebot.types.Message):
-    print('command' + message.text)
-    bot.reply_to(message, f"Привет, {message.chat.username}! Могу пересчитать валюты.")
+    bot.reply_to(message, f"Привет, {message.chat.username}! Для пересчета валют используй выражение в следующем формате:\n"
+                          f"<исходная валюта> <целевая валюта> <количество валюты>\n "
+                          f"Для вывода спиская доступных валют используй команду /values")
+
+@bot.message_handler(commands=['values'])
+def trycommand(message: telebot.types.Message):
+    bot.reply_to(message, f"{message.chat.username}, я могу конвертировать следующие валюты: \n"
+                          f"{', '.join([i for i in calc.legal_currency.values()])}")
 
 @bot.message_handler()
 def trycommand(message: telebot.types.Message):
     print(message.text)
     bot.reply_to(message, f"Hello {message.chat.username}")
 
-bot.polling(none_stop=True)
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
